2d806434b74f7655db5cc9579f01f42c80e38f68
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-02-14 11:27:32 -0600

    Michelle is ready

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('/marc', function() {
     $dragon = Farm::create(\Cowsayphp\Farm\Dragon::class);
     echo '<pre>'.$dragon->say("Marc is ready!").'</pre>';
 });
+
+Route::get('/michelle', function() {
+    $dragon = Farm::create(\Cowsayphp\Farm\Whale::class);
+    echo '<pre>'.$dragon->say("Michelle is ready!").'</pre>';
+});
