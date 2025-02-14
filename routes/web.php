a5ca06e87c5977291d35a28035cc34e2c24900a9
Anton-Yakunin-1 <anton.d.yakunin@gmail.com>
2025-02-14 11:26:27 -0600

    View /Anton to view a cow.

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('/marc', function() {
     $dragon = Farm::create(\Cowsayphp\Farm\Dragon::class);
     echo '<pre>'.$dragon->say("Marc is ready!").'</pre>';
 });
+
+Route::get('/anton', function() {
+    $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
+    echo '<pre>'.$cow->say("Anton is ready!").'</pre>';
+});
