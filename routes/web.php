6ca49e7522ebd7090ceeeb95e4adaf994bdf7283
Lillian Pope <pope.lillian@gmail.com>
2025-02-14 11:23:23 -0600

    Lillian is ready assignment

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,13 @@ Route::get('/marc', function() {
     $dragon = Farm::create(\Cowsayphp\Farm\Dragon::class);
     echo '<pre>'.$dragon->say("Marc is ready!").'</pre>';
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/lillian', function () {
+    $dragon = Farm::create(\Cowsayphp\Farm\dragon::class);
+    echo '<pre>' . $dragon->say("Howdy, Lillian is ready!") . '</pre>';
+});
+
