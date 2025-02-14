91740b9a9fa023a6e9656474784273b5694a808f
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-02-14 11:22:59 -0600

    View /elise to see message

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('/marc', function() {
     $dragon = Farm::create(\Cowsayphp\Farm\Dragon::class);
     echo '<pre>'.$dragon->say("Marc is ready!").'</pre>';
 });
+
+Route::get('/elise', function() {
+    $penguin = Farm::create(\Cowsayphp\Farm\Tux::class);
+    echo '<pre>'.$penguin->say("Elise is ready! (And I'm a penguin now.)").'</pre>';
+});
