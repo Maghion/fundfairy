25c34cdfcab752f785419e32e9674a29081af443
George Medina <geo.f.medina@gmail.com>
2025-02-14 11:26:09 -0600

    roll for initiativ

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('/marc', function() {
     $dragon = Farm::create(\Cowsayphp\Farm\Dragon::class);
     echo '<pre>'.$dragon->say("Marc is ready!").'</pre>';
 });
+
+Route::get('/george/', function($name) {
+    $dragon = Farm::create(\Cowsayphp\Farm\Dragon::class);
+    echo '<pre>'.$dragon->say("Roll for initiative").'</pre>';
+});
