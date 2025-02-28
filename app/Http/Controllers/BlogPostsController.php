b2bc229cfa4a3a46358ff8cd156ebcda7ebe6a91
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-02-28 10:45:35 -0600

    New BlogPostControler and view, Visit /blog-posts, /blog-posts/create, /blog-posts/create/Blog 1

---
+++ app/Http/Controllers/BlogPostsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class BlogPostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        $title = 'BLOG POSTS';
+        $blogPosts = ['Blog 1', 'Blog 2', 'Blog 3'];
+        return view('blog-posts.index', compact('title', 'blogPosts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        $title = 'New Blog Post';
+        return view('blog-posts.create', compact('title'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): string
+    {
+        $title = $request->input('title');
+        $content= $request->input('description');
+
+        return "Title: $title, Description: $content";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): string
+    {
+        return "Showing Blog Posts: $id";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): string
+    {
+        return "Edit Blog Post: $id";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): string
+    {
+        return "You have updated Blog Post: $id";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): string
+    {
+        return "You have deleted Blog Post: $id";
+    }
+}
