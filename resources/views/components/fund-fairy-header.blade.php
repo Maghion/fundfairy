37c3f8fa46d835a819895b941ec44641188e5b35
Anton-Yakunin-1 <anton.d.yakunin@gmail.com>
2025-02-28 12:55:20 -0600

    Touched up Nav-link styles and added mobile formatting to navigation bar.

--- resources/views/components/fund-fairy-header.blade.php
+++ resources/views/components/fund-fairy-header.blade.php
@@ -14,16 +14,16 @@
         </nav>
     </div>
     <!-- Mobile Menu -->
-{{--    <nav--}}
-{{--        id="mobile-menu"--}}
-{{--        class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">--}}
-{{--        <a href="{{url('/about')}}" class="block px-4 py-2 hover:bg-blue-700">About</a>--}}
-{{--        <a href="{{url('/businesses')}}" class="block px-4 py-2 hover:bg-blue-700">Businesses</a>--}}
-{{--        <a href="{{url('/testamonials')}}" class="block px-4 py-2 hover:bg-blue-700">Testamonials</a>--}}
-{{--        <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700">Login</a>--}}
-{{--        <a href="{{url('/donation/create')}}" class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black">--}}
-{{--            <i class="fa fa-edit"></i> Donation Request</a>--}}
-{{--    </nav>--}}
+   <nav id="mobile-menu"
+        class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+       <x-fund-fairy-nav-link :active="request()->is('/')" url="/" :mobile="true">Home</x-fund-fairy-nav-link>
+       <x-fund-fairy-nav-link :active="request()->is('about')" url="/about" :mobile="true">About</x-fund-fairy-nav-link>
+       <x-fund-fairy-nav-link :active="request()->is('businesses')" url="/businesses" :mobile="true">Businesses</x-fund-fairy-nav-link>
+       <x-fund-fairy-nav-link :active="request()->is('businesses/saved')" url="/businesses/saved" :mobile="true">Bookmarks</x-fund-fairy-nav-link>
+       <x-fund-fairy-nav-link :active="request()->is('register')" url="/register" :mobile="true">Register</x-fund-fairy-nav-link>
+       <x-fund-fairy-nav-link :active="request()->is('login')" url="/login" :mobile="true">User Login</x-fund-fairy-nav-link>
+
+    </nav>
 </header>
 
 
