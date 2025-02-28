21cdb2e7a53ffb0bdd99afd455567f0e201bacae
George Medina <geo.f.medina@gmail.com>
2025-02-28 12:05:18 -0600

    Added top banner and hero. Viewable only on home page

--- resources/views/layouts/fund-fairy-layout.blade.php
+++ resources/views/layouts/fund-fairy-layout.blade.php
@@ -6,12 +6,22 @@
     <title>Fund Fairy @if($title)| {{ $title }} @endif</title>
     <link rel="stylesheet" href="https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/styles/themes/default.css" />
     <link rel="stylesheet" href="https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/styles/webawesome.css" />
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" />
     <script type="module" src="https://early.webawesome.com/webawesome@3.0.0-alpha.10/dist/webawesome.loader.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/bf8479ea8b.js" crossorigin="anonymous"></script>
+
 </head>
 <body class="bg-gray-100">
 <x-fund-fairy-header />
-<main class="container mx-auto p-4 mt-4">{{ $slot }}</main>
+
+@if(request() ->is('/'))
+    <x-fund-fairy-top-banner />
+    <x-fund-fairy-hero />
+@endif
+<main class="container mx-auto p-4 mt-4">
+
+    {{ $slot }}
+</main>
 </body>
 </html>
