49193a5b112ccea5b7710ad9cfe1514bd2cbdcf7
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-03-07 12:50:12 -0600

    Updated blog Post migration

--- database/migrations/2025_03_07_155716_create_blog_posts_table.php
+++ database/migrations/2025_03_07_155716_create_blog_posts_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 return new class extends Migration
 {
@@ -15,10 +16,19 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('title');
-            $table->text('context');
+            $table->text('content');
             $table->enum('status', ['published', 'draft'])->default('published');
             $table->timestamp('created_at')->nullable();
         });
+
+        Schema::table('blog_posts', function (Blueprint $table) {
+            // Add this line
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+
+
     }
 
     /**
@@ -27,5 +37,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('blog_posts');
+
+        Schema::table('blog_posts', function (Blueprint $table) {
+            // Drop foreign key constraint and user_id column
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+
+            // ...
+        });
     }
 };
