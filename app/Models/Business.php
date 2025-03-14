54afd2c93c1ce05b2293cad76bdf0a9a2041e351
Anton-Yakunin-1 <anton.d.yakunin@gmail.com>
2025-03-14 09:43:27 -0500

    Generate Model Business

---
+++ app/Models/Business.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Business extends Model
+{
+    protected $table = 'business';
+
+    protected $fillable = [
+        'name',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'zip_code',
+        'phone_number',
+    ];
+}
--- resources/views/businesses/index.blade.php
+++ resources/views/businesses/index.blade.php
@@ -2,7 +2,18 @@
     <x-slot name="title">{{ $title }}</x-slot>
     <ul>
         @forelse($businesses as $business)
-            <li>{{ $business }}</li>
+
+            <li>
+                <a href="{{ route('businesses.show', $business->id) }}">
+                    {{ $business->name }}
+                </a><br>
+                {{ $business->name }}<br>
+                {{ $business->address1 }}<br>
+                {{ $business->address2 }}<br>
+                {{ $business->city }}<br>
+                {{ $business->state }}<br>
+                {{ $business->zip_code }}<br>
+                {{ $business->phone_number }}</li>
         @empty
             <li>No businesses found</li>
         @endforelse
