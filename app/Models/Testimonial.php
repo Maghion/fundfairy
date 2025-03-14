150a52bfd6b1820b6a353fb94ed610c78d79956d
Keluzindya <lakerenluzindya@gmail.com>
2025-03-14 03:33:17 -0500

    Created Testimonial Model,  updated the User model, TestimonialController and index to show the testimonials

---
+++ app/Models/Testimonial.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Testimonial extends Model
+{
+    use HasFactory;
+
+    protected $table = 'testimonials';
+    protected $fillable = [
+        'user_id',
+        'description',
+        'status',
+        'featured'
+    ];
+
+    // Relation to user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
