cc03ee072945ca7339877b9ef6a463b3d8a8bc36
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-03-14 12:00:49 -0500

    created BlogPostFactory

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogPost>
+ */
+class BlogPostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(), // Create new user for each listing
+            'title' => $this->faker->jobTitle,
+            'content' => $this->faker->paragraphs(3, true),
+        ];
+    }
+}
