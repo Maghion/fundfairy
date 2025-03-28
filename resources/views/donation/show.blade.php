a351b8c5803ac51f4d42e6beca2a7ab299d1cd96
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-03-28 10:51:54 -0500

    First Attempt styling donations index and show (view /donation and /donation/show

--- resources/views/donation/show.blade.php
+++ resources/views/donation/show.blade.php
@@ -1,12 +1,23 @@
-<x-fund-fairy-layout>
-    <x-slot name="title">Viewing Donation for {{ number_format($donation->amount, 2) }}</x-slot>
-        @forelse($donations as $donation)
-                <strong>Amount:</strong> ${{ number_format($donation->amount, 2) }}
+{{--<x-fund-fairy-layout>--}}
+{{--    <x-slot name="title">Viewing Donation for {{ number_format($donation->amount, 2) }}</x-slot>--}}
+{{--        @forelse($donations as $donation)--}}
+{{--                <strong>Amount:</strong> ${{ number_format($donation->amount, 2) }}--}}
 {{--            <li><strong>Donor:</strong>--}}
 {{--                {{ $donation->anon }} ? 'Anonymous' : {{ $donation->user->first_name }} . ' ' . {{ $donation->user->last_name }}--}}
 {{--            </li>--}}
 {{--            <li><strong>Request Title:</strong> {{ $donation->donationRequest->title }}</li>--}}
-            <li><strong>Message:</strong> {{ $donation->message }}</li>
-            <li><strong>Type:</strong> {{ ($donation->type) }}</li>
-        @endforelse
+{{--            <li><strong>Message:</strong> {{ $donation->message }}</li>--}}
+{{--            <li><strong>Type:</strong> {{ ($donation->type) }}</li>--}}
+{{--        @endforelse--}}
+{{--</x-fund-fairy-layout>--}}
+
+
+<x-fund-fairy-layout>
+<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$donation->user_id}} donated ${{ number_format($donation->amount, 2) }}</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $donation->message }}</p>
+    <p class="mb-3 font-normal text-gray-700 text-sm dark:text-gray-400">{{ $donation->type }}</p>
+</div>
 </x-fund-fairy-layout>
