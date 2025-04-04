c6a8f3b134cd4e9396f6389fbd230aaae2c1ab2d
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-04-04 12:01:31 -0500

    create a page that shows all blog post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
 
+
 class HomeController extends Controller
 {
     public function index() {
 
         $blogPosts =BlogPost::latest()->limit(6)->get();
-        return view('pages.index');
+        return view('pages.index')->with('blogPosts', $blogPosts)->with('title', $title);
     }
 
     //
