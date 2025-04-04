65cb7f2755c1f6631f56687b4d5cd010e8d4c33f
Lillian Pope <pope.lillian@gmail.com>
2025-04-04 12:34:09 -0500

    Fixes for show page

--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -1,7 +1,6 @@
 
 <x-fund-fairy-layout>
     <h2 class='text-xl font-semibold mt-6'>Comments</h2>
-
         <ul class='mt-4 space-y-4'>
                 <li class='p-4 border rounded-lg shadow-sm'>
                     <p class='font-semibold'>{{ $comment->user->first_name }} {{ $comment->user->last_name }}</p>
@@ -9,7 +8,6 @@
                     <p class='text-sm text-fuchsia-600'>Posted on {{ $comment->created_at->format('M d, Y') }}</p>
                 </li>
         </ul>
-    @endif
 </x-fund-fairy-layout>
 
 
