eb766ab36175f3c845f5e146ce13216a20edadf9
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-04-04 10:16:22 -0500

    create a page that shows all blog post

--- resources/views/blog-posts/show.blade.php
+++ resources/views/blog-posts/show.blade.php
@@ -4,4 +4,9 @@
   <h1 class='text-2xl'>{{  $blogPost->title }}</h1>
   <p>{{  $blogPost->content }}</p>
 
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+
+
+    </div>
+
 </x-fund-fairy-layout>
