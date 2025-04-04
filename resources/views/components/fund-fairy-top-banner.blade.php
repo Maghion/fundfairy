8c5effb9b026fd9053340471823b8ab7997adcb6
Lillian Pope <pope.lillian@gmail.com>
2025-04-04 12:55:11 -0500

    Fixes for show page

--- resources/views/components/fund-fairy-top-banner.blade.php
+++ resources/views/components/fund-fairy-top-banner.blade.php
@@ -1,6 +1,5 @@
 <section class="bg-fuchsia-900 text-white py-6 text-center">
     <div class="container mx-auto">
         <h2 class="text-3xl font-semibold">Fund Your Project Today!</h2>
-
     </div>
 </section>
