6ba91f81009275b9e14b445594b701ca4f1f0143
Lillian Pope <pope.lillian@gmail.com>
2025-04-04 09:49:38 -0500

    Created a CommentCard and updated the CommentController to include what we learned in PHP 2, plus added a some CRUD functions.

--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -6,14 +6,14 @@
     <h2 class='text-xl font-semibold mt-6'>Comments</h2>
 
     @if ($donationRequest->comments->isEmpty())
-        <p class='text-gray-500'>No comments yet.</p>
+        <p class='text-purple-500'>No comments yet.</p>
     @else
         <ul class='mt-4 space-y-4'>
             @foreach ($donationRequest->comments as $comment)
                 <li class='p-4 border rounded-lg shadow-sm'>
                     <p class='font-semibold'>{{ $comment->user->first_name }} {{ $comment->user->last_name }}</p>
-                    <p class='text-gray-700'>{{ $comment->comment }}</p>
-                    <p class='text-sm text-gray-500'>Posted on {{ $comment->created_at->format('M d, Y') }}</p>
+                    <p class='text-purple-700'>{{ $comment->comment }}</p>
+                    <p class='text-sm text-purple-500'>Posted on {{ $comment->created_at->format('M d, Y') }}</p>
                 </li>
             @endforeach
         </ul>
