ad8dbf7588bb1ae56288a1e8cbb1a3cffcd9e335
Lillian Pope <pope.lillian@gmail.com>
2025-04-11 10:26:24 -0500

    updated the comments.show to better style and separate the comments from each other.

--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -1,14 +1,18 @@
 <x-fund-fairy-layout>
     <x-slot name="title">{{ $title }}</x-slot>
-    <ul>
-        @forelse($comments as $comment)
-            <a href="{{ route('comment.show', $comment->id) }}">
-                {{ $comment->title }}
-            </a>
-            <li>{{ $comment->comment }}</li>
-        @empty
-            <li>No comment found</li>
-        @endforelse
 
+    <ul class="mt-4 space-y-4 pl-10 border-l-4 border-fuchsia-300 bg-fuchsia-50 rounded-md">
+        @forelse($comments as $comment)
+            <li class="p-4 bg-white border rounded-lg shadow-sm">
+                <a href="{{ route('comment.show', $comment->id) }}" class="text-fuchsia-900 font-semibold hover:underline">
+                    {{ $comment->title }}
+                </a>
+                <p class="mt-2 italic text-fuchsia-600 pl-4">
+                    {{ $comment->comment }}
+                </p>
+            </li>
+        @empty
+            <li class="p-4 text-gray-500">No comment found</li>
+        @endforelse
     </ul>
 </x-fund-fairy-layout>
--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -1,12 +1,12 @@
 
 <x-fund-fairy-layout>
     <h2 class='text-xl font-semibold mt-6'>Comments</h2>
-    <div class="mt-4 pl-6 border-l-4 border-fuchsia-300 bg-fuchsia-50 rounded-md">
-        <ul class='mt-4 space-y-4'>
+    <div class="mt-4 pl-5 border-l-4 border-fuchsia-300 bg-fuchsia-50 rounded-md">
+        <ul class='space-y-4'>
             <li class="p-4 border rounded-lg shadow-sm bg-white">
-                <p class='font-semibold'>{{ $comment->user->first_name }} {{ $comment->user->last_name }}</p>
-                <p class='text-fuchsia-600'>{{ $comment->comment }}</p>
-                <p class='text-sm text-fuchsia-600'>Posted on {{ $comment->created_at->format('M d, Y') }}</p>
+                <p class='font-semibold text-fuchsia-800'>{{ $comment->user->first_name }} {{ $comment->user->last_name }}</p>
+                <p class='pl-3 italic text-gray-600'>"{{ $comment->comment }}"</p>
+                <p class='text-sm text-fuchsia-950 font-bold'>Posted on {{ $comment->created_at->format('M d, Y') }}</p>
             </li>
         </ul>
     </div>
