169c837b7c7f57f14adc7a00d4ca792d97022e32
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-04-11 11:04:25 -0500

    Login and Register Functionality

--- resources/views/layouts/fund-fairy-layout.blade.php
+++ resources/views/layouts/fund-fairy-layout.blade.php
@@ -23,6 +23,14 @@
     <x-fund-fairy-hero />
 @endif
 <main class="container mx-auto p-4 mt-4">
+    <!-- Display alert messages -->
+    @if (session('success'))
+        <x-alert type="success" message="{{ session('success') }}" />
+    @endif
+
+    @if (session('error'))
+        <x-alert type="error" message="{{ session('error') }}" />
+    @endif
     {{ $slot }}
 </main>
 <x-fund-fairy-bottom-banner />
