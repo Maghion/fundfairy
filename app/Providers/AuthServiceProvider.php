1ad6ef01397f0f4ac16827fcc0a17b9bfce2f93e
SANWIDI <112139535+SANWIDI@users.noreply.github.com>
2025-04-18 11:33:48 -0500

    Create policy and authorization for business review

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,12 +4,16 @@ namespace App\Providers;
 
 use App\Models\Donation;
 use App\Policies\DonationPolicy;
+use App\Models\BusinessReview;
+use App\Policies\BusinessReviewPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Donation::class => DonationPolicy::class,
+        BusinessReview::class => BusinessReviewPolicy::class,
+
     ];
 
     /**
