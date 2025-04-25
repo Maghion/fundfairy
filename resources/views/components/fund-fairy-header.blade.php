fb2158cefc284b1ace0c83b30d60b3cdb246b833
Keluzindya <lakerenluzindya@gmail.com>
2025-04-25 10:19:30 -0500

    Removed testimonials, bookmarks, blog posts adn donation request button from nav bar

--- resources/views/components/fund-fairy-header.blade.php
+++ resources/views/components/fund-fairy-header.blade.php
@@ -8,12 +8,10 @@
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
             <x-fund-fairy-nav-link :active="request()->is('/')" url="/">Home</x-fund-fairy-nav-link>
-            <x-fund-fairy-nav-link :active="request()->is('about')" url="/about">About</x-fund-fairy-nav-link>
-            <x-fund-fairy-nav-link :active="request()->is('blog-posts')" url="/blog-posts">Blog Post</x-fund-fairy-nav-link>
-            <x-fund-fairy-nav-link :active="request()->is('testimonial')" url="/testimonial">Testimonials</x-fund-fairy-nav-link>
             <x-fund-fairy-nav-link :active="request()->is('businesses')" url="/businesses">Businesses</x-fund-fairy-nav-link>
-            <x-fund-fairy-nav-link :active="request()->is('donation-request')" url="/donation-request">Donation Requests</x-fund-fairy-nav-link>
-            @auth
+            <x-fund-fairy-nav-link :active="request()->is('donation-request')" url="/donation-request">Donate</x-fund-fairy-nav-link>
+            <x-fund-fairy-nav-link :active="request()->is('about')" url="/about">About</x-fund-fairy-nav-link>
+        @auth
             <x-fund-fairy-nav-link :active="request()->is('businesses/saved')" url="/businesses/saved">Bookmarks</x-fund-fairy-nav-link>
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
@@ -23,14 +21,8 @@
                 </form>
             @else
                 <x-fund-fairy-nav-link :active="request()->is('register')" url="/register">Register</x-fund-fairy-nav-link>
-                <x-fund-fairy-nav-link :active="request()->is('login')" url="/login">User Login</x-fund-fairy-nav-link>
+                <x-fund-fairy-nav-link :active="request()->is('login')" url="/login">Login</x-fund-fairy-nav-link>
             @endauth
-
-            @auth
-                <x-fund-fairy-button-link btnColor="bg-yellow-500" textClass="text-white" url="/donation-request/create" icon="edit" :block="true">Donation Request</x-fund-fairy-button-link>
-            @endauth
-
-
         </nav>
         <button id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl p-2"></i>
