21b8acfe3c643d06f81f78f71cec075b2f100b92
doucefleur1 <143002182+doucefleur1@users.noreply.github.com>
2025-04-25 11:12:52 -0500

    updated blog post styling, added a back to blogpost  and added time  and author to post.

--- resources/views/blog-posts/edit.blade.php
+++ resources/views/blog-posts/edit.blade.php
@@ -48,7 +48,7 @@
                     @method('DELETE')
                     <button type="submit"
                             class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                        Delete Post
+                      Delete Post
                     </button>
                 </form>
             </div>
