80a4be500ef19638be0e266a15434b7a48d0cc77
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-05-04 18:56:39 -0500

    User Roles for Donations

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -2,30 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleAndPermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Create permissions here
+        // Create permissions with guard_name
+        Permission::firstOrCreate(['name' => 'view-all-donations', 'guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'delete-donations', 'guard_name' => 'web']);
+        Permission::firstOrCreate(['name' => 'edit-donations', 'guard_name' => 'web']);
 
-        $adminRole = Role::create(['name' => 'Admin']);
-        $editorRole = Role::create(['name' => 'Editor']);
+        // Create roles with guard_name
+        $adminRole = Role::firstOrCreate(['name' => 'Admin', 'guard_name' => 'web']);
+        $editorRole = Role::firstOrCreate(['name' => 'Editor', 'guard_name' => 'web']);
 
+        // Assign permissions to roles
         $adminRole->givePermissionTo([
-            // Assign permissions to admin users here
-
+            'view-all-donations',
+            'delete-donations',
+            'edit-donations',
         ]);
 
         $editorRole->givePermissionTo([
-            // Assign permissions to editor users here
-
+            'view-all-donations',
         ]);
     }
 }
